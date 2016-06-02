8371b33d66629c1fa9ef7bae3b6fd774cd05c1df
migueberumenjr <migueberumenjr@gmail.com>
2016-06-02 09:42:44 -0700

    Buttons and transitions
    Buttons only work once and disappear, need to make them work all the time. Created a new transition for the bubbles when they appear

--- pepperjava.js
+++ pepperjava.js
@@ -1,40 +1,98 @@
-  //Define Margin
+//Define Margin
 var width = 1500,
     height = 800,
     padding = 6 // separation between nodes
     maxRadius = 12;
 
-var path,
-    circle,
-    force,
-    nodes,
-    locations,
-    color,
-    m,
-    n,
-    newlocations;
+var path,circle,force,nodes,locations,color,m,n,newlocations;
+
+var bWidth= 100; //button width
+var bHeight= 65; //button height
+var bSpace= 20; //space between buttons
+var x0= 800; //x offset
+var y0= 10; //y offset
+
+//fontawesome button labels
+var labels= ["Color","Origin","Species"];
+
+//colors for different button states 
+var defaultColor= "#FF9922"
+var hoverColor= "#EEEE33"
+var pressedColor= "#FF0000"
 
 var div = d3.select("body").append("div")
     .attr('class', 'tooltip')
     .style("opacity", 0);
 
 var svg = d3.select("body").append("svg")
-          .attr("width", width)
-          .attr("height", height);
+    .attr("width", width)
+    .attr("height", height);
 
+//container for all buttons
+var allButtons= svg.append("g")
+    .attr("id","allButtons") 
 
-$('.tablinks').click(function(event) {
-    if (this.id === "color")  {
-        codeChange("color");
-    }
-    else if (this.id === "origin") {
-        codeChange("origin");
-    }
-    else {
-        codeChange("species");
-    }
-});
-    
+//groups for each button (which will hold a rect and text)
+var buttonGroups= allButtons.selectAll("g.button")
+    .data(labels)
+    .enter()
+    .append("g")
+    .attr("class","button")
+    .style("cursor","pointer")
+    .on("click",function(d,i) {
+        updateButtonColors(d3.select(this), d3.select(this.parentNode))
+        if (d === "Color")  {
+            codeChange("color");
+        }
+        else if (d === "Origin") {
+            codeChange("origin");
+        }
+        else {
+            codeChange("species");
+        }
+    })
+    .on("mouseover", function() {
+        if (d3.select(this).select("rect").attr("fill") != pressedColor) {
+            d3.select(this)
+                .select("rect")
+                .attr("fill",hoverColor);
+        }
+    })
+    .on("mouseout", function() {
+        if (d3.select(this).select("rect").attr("fill") != pressedColor) {
+            d3.select(this)
+                .select("rect")
+                .attr("fill",defaultColor);
+        }
+    })
+
+//adding a rect to each toggle button group
+//rx and ry give the rect rounded corner
+buttonGroups.append("rect")
+    .attr("class","buttonRect")
+    .attr("width",bWidth)
+    .attr("height",bHeight)
+    .attr("x",function(d,i) {return x0+(bWidth+bSpace)*i;})
+    .attr("y",y0)
+    .attr("rx",5) //rx and ry give the buttons rounded corners
+    .attr("ry",5)
+    .attr("fill",defaultColor)
+
+//adding text to each toggle button group, centered 
+//within the toggle button rect
+buttonGroups.append("text")
+    .attr("class","buttonText")
+    .attr("font-family","FontAwesome")
+    .attr("font-size", "22px")
+    .attr("x",function(d,i) {
+        return x0 + (bWidth+bSpace)*i + bWidth/2;
+    })
+    .attr("y",y0+bHeight/2)
+    .attr("text-anchor","middle")
+    .attr("dominant-baseline","central")
+    .attr("fill","black")
+    .text(function(d) {return d;})
+
 function create_graph(category) {
     d3.csv("peppers.csv", function(d) {
         return {
@@ -45,12 +103,13 @@ function create_graph(category) {
             species: d.species,
             color: d.color,
         };
-    }, function(data) {
-        
+    },
+
+    function(data) {
         m = 0;
         n = 0;
-         
-        locations = []
+        locations = [];
+        
         data.forEach(function(d) {
             if (category === "origin") {
                 locations.push(d.origin);
@@ -65,36 +124,30 @@ function create_graph(category) {
                 n = n + 1;
             }
         });
-        
+
         newlocations = locations.filter(function(elem, pos) {
             return locations.indexOf(elem) == pos;
         }); 
-        
+
         m = newlocations.length;
-    
-        
+
         //Define Color
-        
         if (category === "color") {
             color = d3.scale.ordinal()
                 .range(["#FFFF00","#008000","#FF0000", "#FFA500"]);
         }
-        
         else {
-        
-        color = d3.scale.category20()
-            .domain(d3.range(m));
+            color = d3.scale.category20()
+                .domain(d3.range(m));
         }
-        
-        
+
         var x = d3.scale.ordinal()
             .domain(newlocations)
             .rangePoints([0, width], 1),
-            
+
             legend = d3.svg.axis()
-                    .scale(x)
-                    .orient("top")
-        
+                .scale(x)
+                .orient("top")
         
         nodes = data.map(function(d) {
             if (category === "origin")
@@ -104,62 +157,57 @@ function create_graph(category) {
             if (category == "color")
                 var i = newlocations.indexOf(d.color);
             v = d.max;
-          return {
-            name: d.name,
-            min: d.min,
-            max: d.max,
-            avg: (d.min + d.max)/2,
-            origin: d.origin,
-            species: d.species,
-            actcolor: d.color,
-            radius: Math.sqrt(v) * 0.1,
-            color: color(i),
-            cx: x(newlocations[i]),
-            cy: height / 2,
-            image: "http://sweetclipart.com/multisite/sweetclipart/files/red_chili_pepper.png"
-          };
+            return {
+                name: d.name,
+                min: d.min,
+                max: d.max,
+                avg: (d.min + d.max)/2,
+                origin: d.origin,
+                species: d.species,
+                actcolor: d.color,
+                radius: Math.sqrt(v) * 0.1,
+                color: color(i),
+                cx: x(newlocations[i]),
+                cy: height / 2,
+                image: "http://sweetclipart.com/multisite/sweetclipart/files/red_chili_pepper.png"
+            };
         });
-        
-        
-        
+
         force = d3.layout.force()
-          .nodes(nodes)
-          .size([width, height])
-          .gravity(0)
-          .charge(0)
-          .on("tick", tick)
-          .start();
-        
+            .nodes(nodes)
+            .size([width, height])
+            .gravity(0)
+            .charge(0)
+            .on("tick", tick)
+            .start();
+
         var gLegend = svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0, " + height * 0.9 + ")")
-          .call(legend);
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + height * 0.9 + ")")
+            .call(legend);
+
         gLegend.selectAll(".tick text")
-          //.attr("fill", function(d, i) {
-        .attr("fill", function(d, i) {
-            return '#000000';
-          });
-        
-        var reddd;
-        
+            .attr("fill", function(d, i) {
+                return '#000000';
+            });
 
         circle = svg.append("g").selectAll("circle")
-          .data(nodes)
-          .enter().append("circle")
-          .attr("r", function(d) {
-            return d.radius;
-          })
-          .style("fill", function(d){
-            if (d.actcolor == "red"){
-                return '#FF0000';
-            }else if (d.actcolor == "green"){
-                return '#009933';
-            }else if (d.actcolor == "yellow"){
-                return '#ffd633';
-            }else if (d.actcolor == "orange"){
-                return '#ff8000';
-            }
-          })
+            .data(nodes)
+            .enter().append("circle")
+            .style("fill", function(d){
+                if (d.actcolor == "red"){
+                    return '#FF0000';
+                }
+                else if (d.actcolor == "green"){
+                    return '#009933';
+                }
+                else if (d.actcolor == "yellow"){
+                    return '#ffd633';
+                }
+                else if (d.actcolor == "orange"){
+                    return '#ff8000';
+                }
+            })
             .on('mouseover', function(d) {
                 div.transition()
                     .duration(200)
@@ -170,77 +218,148 @@ function create_graph(category) {
             })
             .on('mouseout', function(d) {
                 div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
+                .duration(500)
+                .style("opacity", 0);
             })
-          .call(force.drag);
+            .call(force.drag);
+
+        circle.transition()
+            .duration(1000)
+            .delay(function(d, i) { return i * 10; })
+            .attrTween("r", function(d) {
+                var i = d3.interpolate(0, d.radius);
+                return function(t) { return d.radius = i(t); };
+            });
     });
 }
-   
 
-    function tick(e) {
-      circle
-        .each(gravity(.2 * e.alpha))
+function tick(e) {
+    circle.each(gravity(.2 * e.alpha))
         .each(collide(.5))
         .attr("cx", function(d) {
-          return d.x;
+            return d.x;
         })
         .attr("cy", function(d) {
-          return d.y;
+            return d.y;
         });
-    }
+}
 
-    // Move nodes toward cluster focus.
-    function gravity(alpha) {
-      return function(d) {
+// Move nodes toward cluster focus.
+function gravity(alpha) {
+    return function(d) {
         d.y += (d.cy - d.y) * alpha;
         d.x += (d.cx - d.x) * alpha;
-      };
-    }
+    };
+}
 
-    // Resolve collisions between nodes.
-    function collide(alpha) {
-      var quadtree = d3.geom.quadtree(nodes);
-      return function(d) {
+// Resolve collisions between nodes.
+function collide(alpha) {
+    var quadtree = d3.geom.quadtree(nodes);
+    return function(d) {
         var r = d.radius + maxRadius + padding,
-          nx1 = d.x - r,
-          nx2 = d.x + r,
-          ny1 = d.y - r,
-          ny2 = d.y + r;
+            nx1 = d.x - r,
+            nx2 = d.x + r,
+            ny1 = d.y - r,
+            ny2 = d.y + r;
         quadtree.visit(function(quad, x1, y1, x2, y2) {
-          if (quad.point && (quad.point !== d)) {
-            var x = d.x - quad.point.x,
-              y = d.y - quad.point.y,
-              l = Math.sqrt(x * x + y * y),
-              r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
-            if (l < r) {
-              l = (l - r) / l * alpha;
-              d.x -= x *= l;
-              d.y -= y *= l;
-              quad.point.x += x;
-              quad.point.y += y;
+            if (quad.point && (quad.point !== d)) {
+                var x = d.x - quad.point.x,
+                    y = d.y - quad.point.y,
+                    l = Math.sqrt(x * x + y * y),
+                    r = d.radius + quad.point.radius + (d.color !== quad.point.color) * padding;
+                if (l < r) {
+                    l = (l - r) / l * alpha;
+                    d.x -= x *= l;
+                    d.y -= y *= l;
+                    quad.point.x += x;
+                    quad.point.y += y;
+                }
             }
-          }
-          return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+            return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
         });
-      };
-    }
+    };
+}
 
-    function clear_graph() {
-        nodes = {};
-        links = [];
-        newlocations = [];
-        svg.selectAll('g').remove();
-    }
+function updateButtonColors(button, parent) {
+    parent.selectAll("rect")
+        .attr("fill",defaultColor)
+    button.select("rect")
+        .attr("fill",pressedColor)
+}
 
-    function codeChange(category) {
-        clear_graph();
-        create_graph(category)
-    }
+function clear_graph() {
+    nodes = {};
+    links = [];
+    newlocations = [];
+    svg.selectAll('g').remove();
+}
 
-
-
-
-
-
+function codeChange(category) {
+    clear_graph();
+    create_graph(category);
     
+//container for all buttons
+var allButtons= svg.append("g")
+    .attr("id","allButtons") 
+
+//groups for each button (which will hold a rect and text)
+var buttonGroups= allButtons.selectAll("g.button")
+    .data(labels)
+    .enter()
+    .append("g")
+    .attr("class","button")
+    .style("cursor","pointer")
+    .on("click",function(d,i) {
+        updateButtonColors(d3.select(this), d3.select(this.parentNode))
+        if (d === "Color")  {
+            codeChange("color");
+        }
+        else if (d === "Origin") {
+            codeChange("origin");
+        }
+        else {
+            codeChange("species");
+        }
+    })
+    .on("mouseover", function() {
+        if (d3.select(this).select("rect").attr("fill") != pressedColor) {
+            d3.select(this)
+                .select("rect")
+                .attr("fill",hoverColor);
+        }
+    })
+    .on("mouseout", function() {
+        if (d3.select(this).select("rect").attr("fill") != pressedColor) {
+            d3.select(this)
+                .select("rect")
+                .attr("fill",defaultColor);
+        }
+    })
+
+    //adding a rect to each toggle button group
+    //rx and ry give the rect rounded corner
+    buttonGroups.append("rect")
+        .attr("class","buttonRect")
+        .attr("width",bWidth)
+        .attr("height",bHeight)
+        .attr("x",function(d,i) {return x0+(bWidth+bSpace)*i;})
+        .attr("y",y0)
+        .attr("rx",5) //rx and ry give the buttons rounded corners
+        .attr("ry",5)
+        .attr("fill",defaultColor)
+
+    //adding text to each toggle button group, centered 
+    //within the toggle button rect
+    buttonGroups.append("text")
+        .attr("class","buttonText")
+        .attr("font-family","FontAwesome")
+        .attr("font-size", "22px")
+        .attr("x",function(d,i) {
+            return x0 + (bWidth+bSpace)*i + bWidth/2;
+        })
+        .attr("y",y0+bHeight/2)
+        .attr("text-anchor","middle")
+        .attr("dominant-baseline","central")
+        .attr("fill","black")
+        .text(function(d) {return d;})
+}
